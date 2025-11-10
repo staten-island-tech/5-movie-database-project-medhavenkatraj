62d1d58e648bd050e8f23a9b2790268b213987ab
medhavenkatraj <medhav@nycstudents.net>
2025-11-10 07:16:06 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import json
-## Open the JSON file of movie data
 movies = open("./movies.json", encoding="utf8")
-## create variable "data" that represents the enitre movie list
 data = json.load(movies)
+for index, item in enumerate(data):
+    print(item["title"])
+
+user = input("pick a movie! type in a year ranging from 1961 to 2023! i will print all the movies after that year ")
+if input != 1900:
+    print("movies")
 
