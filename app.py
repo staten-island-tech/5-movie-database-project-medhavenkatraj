21de47b2f941a6911b77376171e791d655580d1d
medhavenkatraj <medhav@nycstudents.net>
2025-11-14 06:30:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ data = json.load(movies)
 for index, item in enumerate(data):
     print(item["title"])
 
-
+def hello_movies():
     input("do you want to see all the movies after 1900? ").lower()
     for index, item in enumerate(data):
         if item >= 1900:
             print(item["year"]) >= 1900
-
+hello_movies()
