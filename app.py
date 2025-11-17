5436eff0bc7ac1cd81fc7c958f7985eec00fe456
medhavenkatraj <medhav@nycstudents.net>
2025-11-17 07:16:18 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-#file 1
-import json
-movies = open("./movies.json", encoding="utf8")
-data = json.load(movies)
-for index, item in enumerate(data):
-    print(item["title"])
 
 """ def hello_movies():
     input("do you want to see all the movies after 1900? ").lower()
@@ -13,9 +7,17 @@ for index, item in enumerate(data):
 hello_movies() """
 
 
+#file 1
+""" import json
+movies = open("./movies.json", encoding="utf8")
+data = json.load(movies)
+for index, item in enumerate(data):
+    print(item["title"])
+
+
 #file 2
 def hello_movies():
-    year = int(input("enter a year: "))
+    year = int(input("enter a year, i will print all the movies releases after that year: "))
     print("movies releases after", year, ":" )
     for item in data:
         if item ["year"] > year:
@@ -26,7 +28,14 @@ hello_movies()
 #file 3 
 
 def hello_movies():
-    year = int(input("enter a year: "))
+    year = int(input("enter a year, i will print all the movies releases after that year: "))
+    print("movies releases after", year, ":" )
+    for item in data:
+        if item ["year"] > year:
+            print(item["title"])
+hello_movies()
+def hello_movies():
+    year = int(input("enter a year, i will print all the movies released before that year: "))
     print("movies released before", year, ":" )
     for item in data:
         if item ["year"] < year:
@@ -37,11 +46,36 @@ hello_movies()
 #file 4 
 
 def hello_movies():
-    year = int(input("enter a year: "))
+    year = int(input("enter a year, i will print all the movies released before that year: "))
     print("movies released before", year, ":" )
     for item in data:
         if item ["year"] < year:
             print(item["title"])
 hello_movies()
+ """
+
+#file 5
+""" movies = open("./movies.json", encoding="utf8")
+import json
+data = json.load(movies)
+
+def hello_movies():
+    answer = input("pick a movie you want to watch ").lower()
+    for index, item in enumerate(data):
+        print(answer["title"])
+    hello_movies()
+import json
+movies = open("./movies.json", encoding="utf8")
+data = json.load(movies)
+def hello_movies():
+    answer = input("pick a movie u would want to watch ").lower()
+    if answer in json == "true":
+        print("answer")  """
+
+
+def hello_movies():
+    movies == str(input("pick a movie that you want to watch "))
+
+
 
 
